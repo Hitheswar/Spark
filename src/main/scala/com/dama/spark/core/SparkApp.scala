3e40f7ce06cd9396bf8c34491d03820985f9fb63
hitheswar <dama.hitheswar@aline-consulting.com>
2020-01-27 17:10:29 +0530

    added new classes

---
+++ src/main/scala/com/dama/spark/core/SparkApp.scala
@@ -0,0 +1,10 @@
+package src.main.scala.com.dama.spark.core
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+trait SparkApp extends App{
+
+
+
+}
---
+++ src/main/scala/com/dama/spark/util/helpar.scala
@@ -0,0 +1,8 @@
+package src.main.scala.com.dama.spark.util
+
+object helpar {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+
+}
