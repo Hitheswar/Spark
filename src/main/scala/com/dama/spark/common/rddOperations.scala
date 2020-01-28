6eecd594d025b006bda31eedeae1e43b89e6486f
hitheswar <dama.hitheswar@aline-consulting.com>
2020-01-28 11:28:27 +0530

    rdd operations

--- src/main/scala/com/dama/spark/common/rddOperations.scala
+++ src/main/scala/com/dama/spark/common/rddOperations.scala
@@ -58,6 +58,65 @@ object rddOperations extends App with SparkApp{
   println("***reduceByKey***")
   val words = Array("one", "two", "two", "three", "three", "three")
   val wordPairsRDD = sc.parallelize(words).map(word => (word, 1))
+  println(wordPairsRDD.collect.toList)
+  val rbk = wordPairsRDD.reduceByKey(_ + _).collect()     //reduceByKey
+  println(rbk.toList)
 
+  val gbk = wordPairsRDD.groupByKey().map(t => (t._1, t._2.sum)).collect()    //groupByKey
+  println(gbk.toList)
+
+  println("***sample***")
+  val x6 = sc.parallelize(Array(1, 2, 3, 4, 5))
+  val y6 = x6.sample(false, 0.4)
+  println(y6.collect.toList)
+
+  println("***union***")
+  val x7 = sc.parallelize(Array(1,2,3), 2)
+  val y7 = sc.parallelize(Array(3,4), 1)
+  val z7 = x7.union(y7)
+  val z77 = z7.glom().collect()
+  println(z77.toList)
+
+  println("***join***")
+  val x8 = sc.parallelize(Array(("a", 1), ("b", 2)))
+  val y8 = sc.parallelize(Array(("a", 3), ("a", 4), ("b", 5)))
+  val z8 = x8.join(y8)
+  println(z8.collect().mkString(", "))
+
+  println("***distinct***")
+  val x9 = sc.parallelize(Array(1,2,3,3,4))
+  val y9 = x.distinct()
+  println(y9.collect().mkString(", "))
+
+  println("***Aggregate***")
+  def seqOp = (data:(Array[Int], Int), item:Int) => (data._1 :+ item, data._2 + item)
+  def combOp = (d1:(Array[Int], Int), d2:(Array[Int], Int)) => (d1._1.union(d2._1), d1._2 + d2._2)
+  val x10 = sc.parallelize(Array(1,2,3,4))
+  val y10 = x10.aggregate((Array[Int](), 0))(seqOp, combOp)
+  println(y10)
+
+  println("***max***")
+  val a1 = sc.parallelize(Array(2,4,1))
+  val b1 = a1.max
+  println(b1)
+
+  println("***sum***")
+  val a2 = sc.parallelize(Array(2,4,1))
+  val b2 = a2.sum
+  println(b2)
+
+  println("***mean***")
+  val a3 = sc.parallelize(Array(2,4,1))
+  val b3 = a3.mean
+  println(b3)
+
+  println("***countByKey***")
+  val a4 = sc.parallelize(Array(('J',"James"),('F',"Fred"), ('A',"Anna"),('J',"John")))
+  val b4 = a4.countByKey()
+  println(y.collect.toList)
+
+  println("***saveAsTextFile***")
+  val a5 = sc.parallelize(Array(2,4,1))
+  a5.saveAsTextFile("H:\\STudy\\test\\text.txt")
 
 }
