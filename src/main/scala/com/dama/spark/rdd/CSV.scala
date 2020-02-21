227bbc0f54a07121cf764d286fb43446886911a0
hitheswar <dama.hitheswar@aline-consulting.com>
2020-02-22 00:09:57 +0530

    reading csv as rdd

--- src/main/scala/com/dama/spark/rdd/CSV.scala
+++ src/main/scala/com/dama/spark/rdd/CSV.scala
@@ -8,9 +8,12 @@ object CSV extends App with SparkApp{
   val file_path = projectPath+"/data/csv/"
 
   val csv_rdd = spark.sparkContext.textFile(file_path+"matches.csv")
-  //csv_rdd.foreach(println)
+  csv_rdd.foreach(println)
   println(csv_rdd.count())
   println(csv_rdd.partitions.size)
+
+  //csv_rdd.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }.foreach(println)
+
 
 
 
